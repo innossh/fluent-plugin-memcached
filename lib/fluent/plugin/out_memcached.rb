c4abeecea170ffb79fd81ecb6e20776b783b1dee
innossh <innossh@users.noreply.github.com>
2015-11-28 02:03:13 +0900

    Support json format

--- lib/fluent/plugin/out_memcached.rb
+++ lib/fluent/plugin/out_memcached.rb
@@ -1,6 +1,14 @@
 class Fluent::MemcachedOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('memcached', self)
-  attr_reader :host, :port, :memcached
+
+  config_param :host, :string, :default => 'localhost'
+  config_param :port, :integer, :default => 11211
+
+  config_param :value_format, :string, :default => 'raw'
+  config_param :param_names, :string, :default => nil # nil doesn't allowed for json
+
+  attr_accessor :memcached
+  attr_accessor :formatter
 
   def initialize
     super
@@ -9,8 +17,10 @@ class Fluent::MemcachedOutput < Fluent::BufferedOutput
 
   def configure(conf)
     super
-    @host = conf.has_key?('host') ? conf['host'] : 'localhost'
-    @port = conf.has_key?('port') ? conf['port'].to_i : 11211
+    if @value_format == 'json' and @param_names.nil?
+      raise Fluent::ConfigError, "param_names MUST be specified in the case of json format"
+    end
+    @formatter = RecordValueFormatter.new(@value_format, @param_names)
   end
 
   def start
@@ -28,10 +38,36 @@ class Fluent::MemcachedOutput < Fluent::BufferedOutput
 
   def write(chunk)
     chunk.msgpack_each { |tag, time, record|
-      key = record.values.first
-      value = record.values.drop(1).join(' ')
-      @memcached.set key, value
+      @memcached.set @formatter.key(record), @formatter.value(record)
     }
+  end
+
+  class RecordValueFormatter
+    attr_reader :value_format
+    attr_reader :param_names
+
+    def initialize(value_format, param_names)
+      @value_format = value_format
+      @param_names = param_names
+    end
+
+    def key(record)
+      record.values.first
+    end
+
+    def value(record)
+      case @value_format
+        when 'json'
+          values = record.values.drop(1)
+          hash = {}
+          @param_names.split(/\s*,\s*/).each_with_index { |param_name, i|
+            hash[param_name] = (i > values.size - 1) ? nil : values[i]
+          }
+          hash.to_json
+        else
+          record.values.drop(1).join(' ')
+      end
+    end
   end
 
 end
--- test/plugin/test_out_memcached.rb
+++ test/plugin/test_out_memcached.rb
@@ -12,6 +12,13 @@ class MemcachedOutputTest < Test::Unit::TestCase
     port 11211
   ]
 
+  CONFIG_JSON = %[
+    host 127.0.0.1
+    port 11211
+    value_format json
+    param_names param1,param2
+  ]
+
   def create_driver(conf = CONFIG, tag='test')
     Fluent::Test::BufferedOutputTestDriver.new(Fluent::MemcachedOutput, tag).configure(conf)
   end
@@ -26,12 +33,27 @@ class MemcachedOutputTest < Test::Unit::TestCase
 
     assert_equal '127.0.0.1', d.instance.host
     assert_equal 11211, d.instance.port
+
+    d = create_driver(CONFIG_JSON)
+
+    assert_equal '127.0.0.1', d.instance.host
+    assert_equal 11211, d.instance.port
+    assert_equal 'json', d.instance.value_format
+    assert_equal 'param1,param2', d.instance.param_names
+
+    assert_raise(Fluent::ConfigError) {
+      create_driver %[
+        host 127.0.0.1
+        port 11211
+        value_format json
+      ]
+    }
   end
 
   def test_format
     d = create_driver
     time = Time.parse('2011-01-02 13:14:15 UTC').to_i
-    record = { 'key' => 'key', 'value1' => 'value' }
+    record = {'key' => 'key', 'param1' => 'value'}
     d.emit(record, time)
     d.expect_format(['test', time, record].to_msgpack)
     d.run
@@ -40,8 +62,8 @@ class MemcachedOutputTest < Test::Unit::TestCase
   def test_write
     d = create_driver
     time = Time.parse('2011-01-02 13:14:15 UTC').to_i
-    record1 = { 'key' => 'a', 'value1' => '1' }
-    record2 = { 'key' => 'b', 'value1' => '2', 'value2' => '3' }
+    record1 = {'key' => 'a', 'param1' => '1'}
+    record2 = {'key' => 'b', 'param1' => '2', 'param2' => '3'}
     d.emit(record1, time)
     d.emit(record2, time)
     d.run
@@ -49,4 +71,20 @@ class MemcachedOutputTest < Test::Unit::TestCase
     assert_equal '1', d.instance.memcached.get('a')
     assert_equal '2 3', d.instance.memcached.get('b')
   end
+
+  def test_write_json
+    d = create_driver(CONFIG_JSON)
+    time = Time.parse('2011-01-02 13:14:15 UTC').to_i
+    record1 = {'key' => 'c', 'param1' => '4'}
+    record2 = {'key' => 'd', 'param1' => '5', 'param2' => '6'}
+    record1_value_json = {'param1' => '4', 'param2' => nil}.to_json
+    record2_value_json = {'param1' => '5', 'param2' => '6'}.to_json
+    d.emit(record1, time)
+    d.emit(record2, time)
+    d.run
+
+    assert_equal record1_value_json, d.instance.memcached.get('c')
+    assert_equal record2_value_json, d.instance.memcached.get('d')
+  end
+
 end
