a210408a3cb7b14858fb702027ae14c4b491034f
innossh <innossh@users.noreply.github.com>
2017-06-10 14:41:53 +0900

    Support formatter plugin

--- lib/fluent/plugin/out_memcached.rb
+++ lib/fluent/plugin/out_memcached.rb
@@ -1,109 +1,81 @@
 require 'dalli'
 require 'fluent/plugin/output'
 
-class Fluent::Plugin::MemcachedOutput < Fluent::Plugin::Output
-  Fluent::Plugin.register_output('memcached', self)
+module Fluent::Plugin
+  class MemcachedOutput < Output
+    Fluent::Plugin.register_output('memcached', self)
 
-  helpers :compat_parameters
+    include Fluent::SetTimeKeyMixin
+    include Fluent::SetTagKeyMixin
 
-  DEFAULT_BUFFER_TYPE = "memory"
+    helpers :inject, :formatter, :compat_parameters
 
-  config_param :host, :string, :default => 'localhost'
-  config_param :port, :integer, :default => 11211
+    DEFAULT_BUFFER_TYPE = 'memory'
+    DEFAULT_FORMAT_TYPE = 'csv'
 
-  config_param :increment, :bool, :default => false
-  config_param :value_separater, :string, :default => ' '
+    config_param :host, :string, :default => 'localhost'
+    config_param :port, :integer, :default => 11211
+    config_param :key, :string
+    config_param :include_key, :bool, :default => false
+    config_param :increment, :bool, :default => false
 
-  config_param :value_format, :string, :default => 'raw'
-  config_param :param_names, :string, :default => nil # nil doesn't allowed for json
-
-  config_section :buffer do
-    config_set_default :@type, DEFAULT_BUFFER_TYPE
-  end
-
-  attr_accessor :memcached
-  attr_accessor :formatter
-
-  def configure(conf)
-    compat_parameters_convert(conf, :buffer)
-    super
-    if @value_format == 'json' and @param_names.nil?
-      raise Fluent::ConfigError, "param_names MUST be specified in the case of json format"
+    config_section :buffer do
+      config_set_default :@type, DEFAULT_BUFFER_TYPE
     end
-    @formatter = RecordValueFormatter.new(@increment, @value_separater, @value_format, @param_names)
-  end
 
-  def start
-    super
-    @memcached = Dalli::Client.new("#{@host}:#{@port}")
-  end
+    config_section :format do
+      config_set_default :@type, DEFAULT_FORMAT_TYPE
+      config_set_default :delimiter, ' '
+      config_set_default :force_quotes, false
+    end
 
-  def shutdown
-    @memcached.close
-    super
-  end
+    attr_accessor :memcached
+    attr_accessor :formatter
 
-  def format(tag, time, record)
-    [time, record].to_msgpack
-  end
+    def configure(conf)
+      compat_parameters_convert(conf, :buffer, :inject, :formatter)
+      super
 
-  def formatted_to_msgpack_binary?
-    true
-  end
+      @formatter = formatter_create
+    end
 
-  def multi_workers_ready?
-    true
-  end
+    def start
+      super
 
-  def write(chunk)
-    chunk.msgpack_each { |time, record|
-      key = @formatter.key(record)
-      value = @formatter.value(record)
-      if @increment
-        if @memcached.get(key) == nil
-          # initialize increment value
-          @memcached.incr(key, 1, nil, 0)
+      @memcached = Dalli::Client.new("#{@host}:#{@port}")
+    end
+
+    def shutdown
+      @memcached.close
+
+      super
+    end
+
+    def format(tag, time, record)
+      record = inject_values_to_record(tag, time, record)
+
+      key = @include_key ? record[@key] : record.delete(@key)
+      [time, key, @formatter.format(tag, time, record).chomp].to_msgpack
+    end
+
+    def formatted_to_msgpack_binary?
+      true
+    end
+
+    def multi_workers_ready?
+      true
+    end
+
+    def write(chunk)
+      chunk.msgpack_each do |time, key, value|
+        unless @increment
+          @memcached.set(key, value)
+          next
         end
-        @memcached.incr(key, amt=value)
 
-      else
-        @memcached.set(key, value)
-      end
-    }
-  end
-
-  class RecordValueFormatter
-    attr_reader :increment
-    attr_reader :value_separater
-    attr_reader :value_format
-    attr_reader :param_names
-
-    def initialize(increment, value_separater, value_format, param_names)
-      @increment = increment
-      @value_separater = value_separater
-      @value_format = value_format
-      @param_names = param_names
-    end
-
-    def key(record)
-      record.values.first
-    end
-
-    def value(record)
-      values = record.values.drop(1)
-      case @value_format
-        when 'json'
-          hash = {}
-          @param_names.split(/\s*,\s*/).each_with_index { |param_name, i|
-            hash[param_name] = (i > values.size - 1) ? nil : values[i]
-          }
-          hash.to_json
-        else
-          return values.first.to_i if @increment
-
-          values.join(@value_separater)
+        @memcached.incr(key, value.to_i, nil, value.to_i)
       end
     end
-  end
 
+  end
 end
--- test/plugin/test_out_memcached.rb
+++ test/plugin/test_out_memcached.rb
@@ -1,4 +1,3 @@
-require 'test/unit'
 require 'fluent/test'
 require 'fluent/test/helpers'
 require 'fluent/test/driver/output'
@@ -9,30 +8,38 @@ class MemcachedOutputTest < Test::Unit::TestCase
 
   def setup
     Fluent::Test.setup
+
+    @d = create_driver
+    # Invalidate all existing cache items before testing
+    Dalli::Client.new("#{@d.instance.host}:#{@d.instance.port}").flush_all
+    @time = event_time('2011-01-02 13:14:15 UTC')
   end
 
   CONFIG = %[
-    host 127.0.0.1
-    port 11211
+    key name
+    fields param1,param2
   ]
 
   CONFIG_JSON = %[
-    host 127.0.0.1
-    port 11211
-    value_format json
-    param_names param1,param2
+    key name
+    include_key true
+    format json
+    include_tag_key true
   ]
 
   CONFIG_INCREMENT = %[
-    host 127.0.0.1
-    port 11211
+    key name
+    format single_value
+    message_key param_incr
     increment true
   ]
 
   CONFIG_MYSQL = %[
-    host 127.0.0.1
-    port 11211
-    value_separater |
+    include_time_key true
+    time_format %s
+    key time
+    fields metrics_name,metrics_value
+    delimiter |
   ]
 
   def create_driver(conf = CONFIG)
@@ -40,114 +47,94 @@ class MemcachedOutputTest < Test::Unit::TestCase
   end
 
   def test_configure
-    d = create_driver('')
-    assert_equal 'localhost', d.instance.host
-    assert_equal 11211, d.instance.port
-    assert_equal false, d.instance.increment
-    assert_equal ' ', d.instance.value_separater
-
-    d = create_driver
-    assert_equal '127.0.0.1', d.instance.host
-    assert_equal 11211, d.instance.port
-
-    d = create_driver(CONFIG_JSON)
-    assert_equal '127.0.0.1', d.instance.host
-    assert_equal 11211, d.instance.port
-    assert_equal 'json', d.instance.value_format
-    assert_equal 'param1,param2', d.instance.param_names
-
     assert_raise(Fluent::ConfigError) {
-      create_driver %[
-        host 127.0.0.1
-        port 11211
-        value_format json
-      ]
+      create_driver('')
     }
 
+    assert_equal 'localhost', @d.instance.host
+    assert_equal 11211, @d.instance.port
+    assert_equal false, @d.instance.increment
+    assert_equal ' ', @d.instance.formatter.delimiter
+    assert_equal 'name', @d.instance.key
+    assert_equal ['param1', 'param2'], @d.instance.formatter.fields
+
+    d = create_driver(CONFIG_JSON)
+    assert_equal 'json', d.instance.formatter_configs.first[:@type]
+
     d = create_driver(CONFIG_INCREMENT)
-    assert_equal '127.0.0.1', d.instance.host
-    assert_equal 11211, d.instance.port
     assert_equal true, d.instance.increment
+    assert_equal 'single_value', d.instance.formatter_configs.first[:@type]
+    assert_equal 'param_incr', d.instance.formatter.message_key
 
     d = create_driver(CONFIG_MYSQL)
-    assert_equal '127.0.0.1', d.instance.host
-    assert_equal 11211, d.instance.port
-    assert_equal '|', d.instance.value_separater
+    assert_equal 'time', d.instance.key
+    assert_equal true, d.instance.include_time_key
+    assert_equal '%s', d.instance.inject_config.time_format
+    assert_equal ['metrics_name', 'metrics_value'], d.instance.formatter.fields
+    assert_equal '|', d.instance.formatter.delimiter
   end
 
   def test_format
-    d = create_driver
-    time = Time.parse('2011-01-02 13:14:15 UTC').to_i
-    record = {'key' => 'key', 'param1' => 'value'}
-    d.run(default_tag: 'test') do
-      d.feed(time, record)
+    record = {'name' => 'key', 'param1' => 'value1', 'param2' => 'value2'}
+    @d.run(default_tag: 'test') do
+      @d.feed(@time, record)
     end
-    assert_equal [[time, record].to_msgpack], d.formatted
+    assert_equal [[@time.to_i, 'key', 'value1 value2'].to_msgpack], @d.formatted
   end
 
   def test_write
-    d = create_driver
-    time = event_time('2011-01-02 13:14:15 UTC')
-    record1 = {'key' => 'a', 'param1' => '1'}
-    record2 = {'key' => 'b', 'param1' => '2', 'param2' => '3'}
-    d.run(default_tag: 'test') do
-      d.feed(time, record1)
-      d.feed(time, record2)
+    @d = create_driver
+    record1 = {'name' => 'a', 'param1' => '1'}
+    record2 = {'name' => 'b', 'param1' => '2', 'param2' => '3'}
+    @d.run(default_tag: 'test') do
+      @d.feed(@time, record1)
+      @d.feed(@time, record2)
     end
 
-    assert_equal '1', d.instance.memcached.get('a')
-    assert_equal '2 3', d.instance.memcached.get('b')
+    assert_equal '1 ', @d.instance.memcached.get('a')
+    assert_equal '2 3', @d.instance.memcached.get('b')
   end
 
   def test_write_json
     d = create_driver(CONFIG_JSON)
-    time = event_time('2011-01-02 13:14:15 UTC')
-    record1 = {'key' => 'c', 'param1' => '4'}
-    record2 = {'key' => 'd', 'param1' => '5', 'param2' => '6'}
-    record1_value_json = {'param1' => '4', 'param2' => nil}.to_json
-    record2_value_json = {'param1' => '5', 'param2' => '6'}.to_json
+    record1 = {'name' => 'a', 'param1' => '4'}
+    record2 = {'name' => 'b', 'param1' => '5', 'param2' => '6'}
+    record1_value_json = {'name' => 'a', 'param1' => '4', 'tag' => 'test'}.to_json
+    record2_value_json = {'name' => 'b', 'param1' => '5', 'param2' => '6', 'tag' => 'test'}.to_json
     d.run(default_tag: 'test') do
-      d.feed(time, record1)
-      d.feed(time, record2)
+      d.feed(@time, record1)
+      d.feed(@time, record2)
     end
 
-    assert_equal record1_value_json, d.instance.memcached.get('c')
-    assert_equal record2_value_json, d.instance.memcached.get('d')
+    assert_equal record1_value_json, d.instance.memcached.get('a')
+    assert_equal record2_value_json, d.instance.memcached.get('b')
   end
 
-  class IncrementTest < self
-    def teardown
-      @d.instance.memcached.flush_all
+  def test_write_increment
+    d = create_driver(CONFIG_INCREMENT)
+    record1 = {'name' => 'count1', 'param_incr' => 1}
+    record2 = {'name' => 'count2', 'param_incr' => 2}
+    record3 = {'name' => 'count1', 'param_incr' => 3}
+    record4 = {'name' => 'count2', 'param_incr' => 4}
+    d.run(default_tag: 'test') do
+      d.feed(@time, record1)
+      d.feed(@time, record2)
+      d.feed(@time, record3)
+      d.feed(@time, record4)
     end
 
-    def test_write_increment
-      @d = create_driver(CONFIG_INCREMENT)
-      time = event_time('2011-01-02 13:14:15 UTC')
-      record1 = {'key' => 'count1', 'param1' => 1}
-      record2 = {'key' => 'count2', 'param1' => 2}
-      record3 = {'key' => 'count1', 'param1' => 3}
-      record4 = {'key' => 'count2', 'param1' => 4}
-      @d.run(default_tag: 'test') do
-        @d.feed(time, record1)
-        @d.feed(time, record2)
-        @d.feed(time, record3)
-        @d.feed(time, record4)
-      end
-
-      assert_equal (1 + 3), @d.instance.memcached.get('count1').to_i
-      assert_equal (2 + 4), @d.instance.memcached.get('count2').to_i
-    end
+    assert_equal (1 + 3), d.instance.memcached.get('count1').to_i
+    assert_equal (2 + 4), d.instance.memcached.get('count2').to_i
   end
 
   def test_write_to_mysql
     d = create_driver(CONFIG_MYSQL)
-    time = event_time('2011-01-02 13:14:15 UTC')
-    record = {'key' => time, 'metrics_name' => 'count', 'metrics_value' => '100'}
+    record = {'metrics_name' => 'count', 'metrics_value' => '100'}
     d.run(default_tag: 'test') do
-      d.feed(time, record)
+      d.feed(@time, record)
     end
 
-    assert_equal 'count|100', d.instance.memcached.get(time)
+    assert_equal 'count|100', d.instance.memcached.get(@time.to_i)
   end
 
 end
